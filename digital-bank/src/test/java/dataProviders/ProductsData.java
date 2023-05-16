dc66e4f441a1b845158c5e7f30ae0192374323cf
Andrés Galván <8il.andre@gmail.com>
2023-05-16 11:51:57 -0300

    First commit

---
+++ digital-bank/src/test/java/dataProviders/ProductsData.java
@@ -0,0 +1,13 @@
+package dataProviders;
+
+import org.testng.annotations.DataProvider;
+
+public class ProductsData {
+    @DataProvider(name = "products")
+    public static Object[][] getProductsData() {
+        return new Object[][]{
+                {"Tees, Knits and Polos", "CORE STRIPED SPORT SHIRT", "Indigo", "S"},
+                {"Pants & Denim", "FLAT FRONT TROUSER", "Silver", "31"},
+        };
+    }
+}
---
+++ digital-bank/src/test/java/scripts/AddProductToCartTest.java
@@ -0,0 +1,82 @@
+package scripts;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+
+public class AddProductToCartTest {
+    private WebDriver driver;
+    private Actions actions;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        actions = new Actions(driver);
+        String baseUrl = "http://magento-demo.lexiconn.com/";
+        driver.get(baseUrl);
+    }
+
+//    @Test(dataProvider = "products", dataProviderClass = dataProviders.ProductsData.class)
+//    public void testAddToCart(String subCategory, String product, String productColor, String productSize) {
+//        String expectedTitle = "SHOPPING CART";
+//
+//        CategoryMenu categoryMenu = new CategoryMenu(driver, actions);
+//
+//        ProductSubCategoryPage subCategoryPage = categoryMenu.clickMenSubCategory(subCategory);
+//        assertEquals(subCategoryPage.getSubCategoryPageTitle(), subCategory.toUpperCase());
+//
+//        ProductPage productPage = subCategoryPage.selectProduct(product);
+//        assertEquals(productPage.getProductPageTitle(), product);
+//
+//        productPage.selectColorAndSize(productColor, productSize);
+//        CartPage cartPage = productPage.addToCart();
+//        assertEquals(cartPage.getCartPageTitle(), expectedTitle);
+//        assertEquals(cartPage.getProductName(product), product);
+//
+//        takeScreenshot();
+//    }
+
+    @AfterTest
+    public void tearDown() {
+        try {
+            if (driver != null) {
+                driver.quit();
+            }
+        } catch (Exception e) {
+            System.out.println("Exception while closing the driver " + e.getMessage());
+        }
+    }
+
+    @Attachment(type = "image/png")
+    @AfterMethod(alwaysRun = true)
+    public byte[] takeScreenshot() {
+        byte[] image = new byte[0];
+        try {
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            image = screenshot.getScreenshotAs(OutputType.BYTES);
+            System.out.println("Successfully captured a screenshot");
+        } catch (Exception e) {
+            System.out.println("Exception while taking screenshot " + e.getMessage());
+        }
+        return image;
+    }
+}
